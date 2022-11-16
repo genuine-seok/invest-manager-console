f8ef0db6bb6e0ca526f3372c2417792124cf6c41
genuine-seok <genuineseok@gmail.com>
2022-11-16 14:39:11 +0900

    Feat: json-server 데이터 셋업 및 설정 파일 업데이트
    - next.config 확장자 변경 (js->cjs)
    - 모듈 시스템 관련 config 적용

---
+++ server.ts
@@ -0,0 +1,25 @@
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+
+const jsonServer = require("json-server");
+
+const auth = require("json-server-auth");
+
+const app = jsonServer.create();
+const router = jsonServer.router("db.json");
+
+app.db = router.db;
+const rules = auth.rewriter({
+  users: 660,
+  userSetting: 660,
+  accounts: 660,
+});
+
+app.use(rules);
+app.use(auth);
+app.use(router);
+
+app.listen(4000, () => {
+  console.log("JSON Server is running...");
+});
