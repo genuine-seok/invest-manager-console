7b613b1921e2564f5cfa0efb03c1b8c18e0a44cb
genuine-seok <genuineseok@gmail.com>
2022-11-18 01:51:16 +0900

    Rename: 프로젝트 구조 변경
    - src, data 폴더 추가 및 관련 로직 이동

--- server.ts
+++ server.ts
@@ -3,6 +3,7 @@ import { createRequire } from "module";
 const require = createRequire(import.meta.url);
 
 const jsonServer = require("json-server");
+const cors = require("cors");
 
 const auth = require("json-server-auth");
 
@@ -17,6 +18,7 @@ const rules = auth.rewriter({
 });
 
 app.use(rules);
+app.use(cors());
 app.use(auth);
 app.use(router);
 
